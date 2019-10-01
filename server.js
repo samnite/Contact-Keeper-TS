aabef32aeca7c6c0a141167c6eaee705c9a89589
Alexander Gribenchenko <samnite87@gmail.com>
2019-10-01 11:13:50 +0300

    Included user validation. #38

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/',(req, res) => res.json({msg: 'Welcome to the ContactKeeper API...'}));
+// Connect database
+connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the ContactKeeper API...' })
+);
 
 // Define routes
 app.use('/api/users', require('./routes/users'));
